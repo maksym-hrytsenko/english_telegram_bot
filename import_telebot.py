3cc6eef08f57bae045734df6b15a95483cac0720
MaKsYmKa28 <mgricenko167@gmail.com>
2024-12-16 13:41:48 +0100

    Update import_telebot.py

--- import_telebot.py
+++ import_telebot.py
@@ -6,7 +6,7 @@ from deep_translator import GoogleTranslator
 from github import Github
 WORD_FILE_PATH = r"unlearned_words.txt"
 
-GITHUB_TOKEN = "8debfd6fc5280c9d4aed1f1b9260d3081820f332"  # Замість "ваш_токен" вставте реальний токен
+GITHUB_TOKEN = "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"  # Замість "ваш_токен" вставте реальний токен
 REPO_NAME = "MaKsYmKa28/english_telegram_bot"  # Наприклад: "username/my-bot-repo"
 FILE_PATH = "https://raw.githubusercontent.com/MaKsYmKa28/english_telegram_bot/refs/heads/main/unlearned_words.txt?token=GHSAT0AAAAAAC3ZIWXKAGDVT5GK4HZ2XODYZ3ADDKA"  # Шлях до вашого файлу у репозиторії
 
